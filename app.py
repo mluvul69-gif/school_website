e34afd4adb3e6c70a31b7dc3c8b5ae4a6e3035c8
aliuse <mluvul69@gmail.com>
2026-01-08 15:11:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -127,7 +127,7 @@ def sports():
         current_year=datetime.now().year
     )
 
-@app.route("/report")
+@app.route("/reports")
 def report():
     posts = fetch_posts_by_category("Reports")
     return render_template(
@@ -269,5 +269,6 @@ def delete_post(post_id):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
 
